8517cacd22b65fdb3e08140662581b3a2e949513
Gabe Witcher <gabewitcher@gmail.com>
2025-03-21 20:17:58 -1000

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Gabe Witcher on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
