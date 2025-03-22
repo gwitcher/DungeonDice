43c0d3e43c6fe2a6ab6ccbc6d9d450924bcaac98
Gabe Witcher <gabewitcher@gmail.com>
2025-03-22 15:38:56 -0700

    calculated button rows

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -33,52 +33,83 @@ struct ContentView: View {
     }
     
     @State private var resultMessage = ""
-    @State private var  columns = [
-        GridItem(.adaptive(minimum: 102)),
-    ]
+    @State private var buttonsLeftOver = 0 // # of buttons in a less than full row
+    let horizontalPadding: CGFloat = 16
+    let spacing: CGFloat = 0 // betwen buttons
+    let buttonWidth: CGFloat = 102
+    
+//    @State private var  columns = [
+//        GridItem(.adaptive(minimum: buttonWidth)),
+//    ]
     @State private var animationTrigger = false
     @State private var isDoneAnimating = true
     
     
     var body: some View {
-        VStack {
-            Text("Dungeon Dice")
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .foregroundStyle(.red)
-            
-            Spacer()
-            
-            Text(resultMessage)
-                .font(.largeTitle)
-                .fontWeight(.medium)
-                .frame(height: 150)
-                .multilineTextAlignment(.center)
-//                .scaleEffect(isDoneAnimating ? 1.0 : 0.5)
-//                .opacity(isDoneAnimating ? 1.0 : 0.2)
-                .rotation3DEffect(isDoneAnimating ? .degrees(360) : .degrees(0), axis: (x: 1, y: 0, z: 0))
-                .onChange(of: animationTrigger) {
-                    isDoneAnimating = false
-                    withAnimation(.interpolatingSpring(duration: 0.6, bounce: 0.4)) {
-                        isDoneAnimating = true
+        GeometryReader { geo in
+            VStack {
+                Text("Dungeon Dice")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundStyle(.red)
+                
+                Spacer()
+                
+                Text(resultMessage)
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+                    .frame(height: 150)
+                    .multilineTextAlignment(.center)
+    //                .scaleEffect(isDoneAnimating ? 1.0 : 0.5)
+    //                .opacity(isDoneAnimating ? 1.0 : 0.2)
+                    .rotation3DEffect(isDoneAnimating ? .degrees(360) : .degrees(0), axis: (x: 1, y: 0, z: 0))
+                    .onChange(of: animationTrigger) {
+                        isDoneAnimating = false
+                        withAnimation(.interpolatingSpring(duration: 0.6, bounce: 0.4)) {
+                            isDoneAnimating = true
+                        }
+                    }
+                
+                Spacer()
+                
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: buttonWidth), spacing: spacing)]) {
+                    ForEach(Dice.allCases.dropLast(buttonsLeftOver)) { dice in
+                        Button(dice.description) {
+                            resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+                            animationTrigger.toggle()
+                        }
+                        .frame(width: buttonWidth)
                     }
                 }
-            
-            Spacer()
-            
-            LazyVGrid(columns: columns) {
-                ForEach(Dice.allCases) { dice in
-                    Button(dice.description) {
-                        resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
-                        animationTrigger.toggle()
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+                
+                HStack {
+                    ForEach(Dice.allCases.suffix(buttonsLeftOver)) { dice in
+                        Button(dice.description) {
+                            resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+                            animationTrigger.toggle()
+                        }
+                        .frame(width: buttonWidth)
                     }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.red)
                 }
+                
             }
-            .buttonStyle(.borderedProminent)
-            .tint(.red)
-            
+            .padding()
+            .onAppear {
+                var screenWidth = geo.size.width - horizontalPadding*2 //padding on both sides of buttons
+                if Dice.allCases.count > 1 {
+                    screenWidth += spacing
+                }
+                //calculate number of buttons per row as Int
+                let numberOfButtonsPerRow = Int(screenWidth) / Int(buttonWidth + spacing)
+                buttonsLeftOver = Dice.allCases.count % numberOfButtonsPerRow
+                print("Buttons left over = \(buttonsLeftOver)")
+            }
         }
-        .padding()
+        
     }
 }
 
